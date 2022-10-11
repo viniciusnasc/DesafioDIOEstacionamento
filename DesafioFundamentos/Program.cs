6a16936a5af3796ab7e752751feda5908be668c6
Vinicius Nascimento <83601645+viniciusnasc@users.noreply.github.com>
2022-10-11 11:54:02 -0300

    Inclui a sln

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,8 +3,7 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoInicial = 0, precoPorHora = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
